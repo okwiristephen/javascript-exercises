2b84e20106c9b7f913698cc3d97bcd0b7a2cb0be
okwiristephen <okwirialal@gmail.com>
2019-11-22 13:35:24 +0300

    Shadowing

--- 2-functions/functions.js
+++ 2-functions/functions.js
@@ -278,7 +278,7 @@ describe("functions", function() {
                 // This means that we can't change it.
                 // Is there a way to still make 'myConst' equal true here?
                 // ✏️ ADD CODE HERE ✏️
-
+                const myConst = true;
                 ////////////////////////////////////////////////////////////////////
 
                 if (myConst !== true) {
