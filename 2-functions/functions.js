f881dab86ae473118f1ce69919bc89ba2943dd6b
okwiristephen <okwirialal@gmail.com>
2019-11-22 13:32:18 +0300

    Called on an objec

--- 2-functions/functions.js
+++ 2-functions/functions.js
@@ -138,7 +138,8 @@ describe("functions", function() {
             // and 'answerSecondResult' with the correct results.
             // If you call getContext() here, you're a cheater! :)
             // ✏️ ADD CODE HERE ✏️
-
+            const answerFirstResult =object;
+            const answerSecondResult = differentObject;
             ////////////////////////////////////////////////////////////////////
 
             // First we call the function on an object
