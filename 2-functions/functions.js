7170c1762c4ef5769db8dd70026f4f3e04205754
okwiristephen <okwirialal@gmail.com>
2019-11-22 13:36:37 +0300

    A function that returns a function

--- 2-functions/functions.js
+++ 2-functions/functions.js
@@ -315,7 +315,9 @@ describe("functions", function() {
             ////////////////////////////////////////////////////////////////////
             // Can you implement a function 'myFunction' that returns a function?
             // ✏️ ADD CODE HERE ✏️
-
+            function myFunction(){
+                return function(){};
+            }
             ////////////////////////////////////////////////////////////////////
 
             const result = myFunction();
