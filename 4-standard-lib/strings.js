2671f8f190f06bf402121d762cb79d29c30dcd0b
okwiristephen <okwirialal@gmail.com>
2019-11-29 08:04:52 +0300

    Get substring from string

--- 4-standard-lib/strings.js
+++ 4-standard-lib/strings.js
@@ -123,6 +123,9 @@ describe("strings", function() {
 
             ////////////////////////////////////////////////////////////////////
             // ✏️ ADD CODE HERE. USE THE STANDARD LIB! ✏️
+                function cut(number,x){
+                    return text.slice(number,x);
+                } 
 
             ////////////////////////////////////////////////////////////////////
 
