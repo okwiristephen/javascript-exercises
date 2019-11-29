d1ad8fd7c8765c00030ce67c8bdf3d6e318a5117
okwiristephen <okwirialal@gmail.com>
2019-11-29 08:14:47 +0300

    Get letter at certain position in strin

--- 4-standard-lib/numbers.js
+++ 4-standard-lib/numbers.js
@@ -57,6 +57,9 @@ describe("numbers", function() {
         it("should run without errors", function() {
             ////////////////////////////////////////////////////////////////////////
             // ✏️ ADD CODE HERE. USE THE STANDARD LIB! ✏️
+            function roundNumber(number){
+                return Math.round(number);
+            }
 
             ////////////////////////////////////////////////////////////////////////
 
